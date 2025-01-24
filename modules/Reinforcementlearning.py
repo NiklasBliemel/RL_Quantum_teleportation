0af6c7f60e6502fe5e8b52cf80beb55a68f2e45e
Niklas Bliemel <niklas.bliemel@stud.uni-regensburg.de>
2025-01-24 15:07:03 +0100

    fix SemiGradSarsa

---
+++ modules/Reinforcementlearning.py
@@ -0,0 +1,105 @@
+import torch
+import torch.nn as nn
+import random
+from IPython.display import clear_output
+import matplotlib.pyplot as plt
+
+
+class Tile:
+    def __init__(self, grid_size, N_grids, left_bound=-1, right_bound=1):
+        self.max_index = grid_size - 1
+        self.N_grids = N_grids
+        self.d = 1 / N_grids
+        self.scale = (right_bound - left_bound)
+        self.shift = -left_bound
+        
+    def __call__(self, x):
+        out = (x + self.shift) / self.scale
+        out = self.max_index * out.unsqueeze(0) + (self.d * torch.arange(self.N_grids).unsqueeze(-1))
+        out = torch.trunc(out).int()
+        out[out > self.max_index] = self.max_index
+        out[out < 0] = 0
+        return torch.matmul(out, (self.max_index + 1)**torch.arange(len(x)).int())
+
+
+# test_tile = Tile(5, 8)
+
+# def f(x,y):
+#     plt.plot(x, y, marker="o")
+#     plt.grid(True)
+#     plt.xticks(torch.linspace(-1,1,5))
+#     plt.yticks(torch.linspace(-1,1,5))
+#     plt.title(str(test_tile(torch.tensor([x, y]))))
+#     plt.show()
+
+# ipywidgets.interact(f, x=(-1.0,1.0, 0.01), y=(-1.0,1.0, 0.01))
+
+
+class SemiGradSarsa:
+    def __init__(self, env, q_net, max_steps = 1000, eps = 0.1, gamma = 1., alpha = 0.1):
+        self.env = env
+        self.q_net = q_net
+        self.max_steps = max_steps
+        self.eps = eps 
+        self.gamma = gamma
+        self.alpha = alpha
+        self.log = []
+
+    def update_weights(self, scale):
+        for w in self.q_net.parameters():
+            with torch.no_grad():
+                w += scale * w.grad
+
+    def eps_greedy(self, eps, q_values):
+        if random.random() < eps or torch.std(q_values) == 0:
+            A = random.randint(0, len(q_values) - 1)
+            return A
+        else:
+            return torch.argmax(q_values)
+
+    def plot_log(self):
+        plt.plot(self.log)
+        plt.xlabel("Episodes")
+        plt.ylabel("Steps")
+        # plt.yscale("log")
+        plt.show()
+
+    def run(self, max_episode):
+        try:
+            for ep in range(max_episode):
+                S = self.env.setup()
+                q_values = self.q_net(S)
+                A = self.eps_greedy(self.eps, q_values)
+                self.q_net.zero_grad()
+                q_values[A].backward()
+                for step in range(self.max_steps):
+                    
+                    S_new, R, goal_reached = self.env.step(S, A)
+                    
+                    if goal_reached:
+                        scale = self.alpha * (R - q_values[A])
+                        self.update_weights(scale)
+                        break
+            
+                    q_values_new = self.q_net(S_new)
+                    A_new = self.eps_greedy(self.eps, q_values_new)
+                    scale = self.alpha * (R + self.gamma * q_values_new[A_new] - q_values[A])
+                    self.update_weights(scale)
+        
+                    A = A_new
+                    if isinstance(S, torch.Tensor):
+                        S = S_new.detach().clone()
+                    else:
+                        S = S_new * 1
+                    self.q_net.zero_grad()
+                    q_values = self.q_net(S)
+                    q_values[A].backward()
+                    
+                self.log.append(step+1)
+                if ep%10==0:
+                    clear_output(wait=True)
+                    self.plot_log()
+        except KeyboardInterrupt:
+            clear_output(wait=True)
+            self.plot_log()
+
